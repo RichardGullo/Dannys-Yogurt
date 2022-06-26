7070cfa7a334f56012b2ad46e353e902d8f6e374
Richard B Gullo <richard_gullo@knights.ucf.edu>
2022-06-26 00:40:39 -0400

    Updated navbar responsiveness
    Responsiveness added to navbar.

--- dannys-yogurt/app/public/wp-content/themes/danny-yogurt-theme/header.php
+++ dannys-yogurt/app/public/wp-content/themes/danny-yogurt-theme/header.php
@@ -8,10 +8,10 @@
 
 <body <?php body_class(); ?>>
 
-<div class="container main-container border" style="min-height:100vh;">
+<div class="container-md border" style="min-height:100vh;">
 
 <!--Nav -->
-<nav class="navbar navbar-expand-sm navbar-light">
+<nav class="navbar navbar-expand-md navbar-light">
     <a class="navbar-brand" href="<?php echo site_url('/home'); ?>"><img src="<?php echo get_theme_file_uri('images/logo.svg') ?>"</a>
     <a id="logo" href="#">Danny's Yogurt</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
